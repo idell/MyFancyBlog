1425dd3a9d6ef95c7fe1677ef24bb5fb3a5a0100
Ivan Dell'Oro <ivan.delloro@lastminute.com>
2024-03-29 16:27:10 +0100

    removed unused import

--- src/main/kotlin/idell/projects/blog/crud/MyFancyBlogCrudController.kt
+++ src/main/kotlin/idell/projects/blog/crud/MyFancyBlogCrudController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
-import java.net.URI
 
 @RestController
 class MyFancyBlogCrudController(private val blogPostUseCase: BlogPostUseCase, private val requestAdapter:BlogCrudRequestAdapter) {
