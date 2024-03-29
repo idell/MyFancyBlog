60eaf7ec2a19e2c822e8c44fa22fa36f495c1a56
Ivan Dell'Oro <ivan.delloro@lastminute.com>
2024-03-29 12:06:23 +0100

    handling authentication

--- src/main/kotlin/idell/projects/blog/create/MyFancyBlogCrudController.kt
+++ src/main/kotlin/idell/projects/blog/create/MyFancyBlogCrudController.kt
@@ -1,8 +1,10 @@
 package idell.projects.blog.create
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
@@ -10,11 +12,19 @@ import java.net.URI
 class MyFancyBlogCrudController(private val blogPostUseCase: BlogPostUseCase) {
 
     @PostMapping("/v1/create/")
-    fun createPost(@RequestBody blogPostRequest: BlogPostRequest): ResponseEntity<Any> {
+    fun createPost(@RequestHeader("X-User") user:String, @RequestBody blogPostRequest: BlogPostRequest): ResponseEntity<Any> {
+        if (!ENABLED_USERS.contains(user)){
+            return ResponseEntity(HttpStatus.UNAUTHORIZED)
+        }
+
         return when (val blogPost = blogPostUseCase.publish(blogPostRequest)) {
             is BlogPostCreated -> ResponseEntity.created(URI.create(blogPost.uri)).build()
             is BlogPostAlreadyPresent -> ResponseEntity.badRequest().build()
         }
+    }
+
+    companion object{
+        private val ENABLED_USERS = listOf("user","admin")
     }
 }
 
--- src/test/kotlin/idell/projects/blog/create/MyFancyBlogCrudControllerTest.kt
+++ src/test/kotlin/idell/projects/blog/create/MyFancyBlogCrudControllerTest.kt
@@ -1,10 +1,10 @@
 package idell.projects.blog.create
 
-import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.times
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import java.net.URI
 
@@ -19,7 +19,7 @@ class MyFancyBlogCrudControllerTest {
         Mockito.`when`(blogPostUseCase.publish(BlogPostCreateRequest("aTitle")))
                 .thenReturn(BlogPostCreated("aUri"))
 
-        val actual: ResponseEntity<Any> = underTest.createPost(BlogPostCreateRequest("aTitle"))
+        val actual: ResponseEntity<Any> = underTest.createPost("user", BlogPostCreateRequest("aTitle"))
         val expected = ResponseEntity.created(URI.create("aUri")).build<Any>()
 
         Mockito.verify(blogPostUseCase, times(1)).publish(BlogPostCreateRequest("aTitle"))
@@ -32,10 +32,20 @@ class MyFancyBlogCrudControllerTest {
         Mockito.`when`(blogPostUseCase.publish(BlogPostCreateRequest("anAlreadyPresentPost")))
                 .thenReturn(BlogPostAlreadyPresent)
 
-        val actual: ResponseEntity<Any> = underTest.createPost(BlogPostCreateRequest("anAlreadyPresentPost"))
+        val actual: ResponseEntity<Any> = underTest.createPost("user", BlogPostCreateRequest("anAlreadyPresentPost"))
         val expected = ResponseEntity.badRequest().build<Any>()
 
         Mockito.verify(blogPostUseCase, times(1)).publish(BlogPostCreateRequest("anAlreadyPresentPost"))
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `will answer 401 if the user is not recognized`() {
+
+        val actual: ResponseEntity<Any> = underTest.createPost("aNotAutorizedUser", BlogPostCreateRequest("aTitle"))
+        val expected = ResponseEntity<HttpStatus>(HttpStatus.UNAUTHORIZED)
+
+        Mockito.verifyNoInteractions(blogPostUseCase)
         Assertions.assertThat(actual).isEqualTo(expected)
     }
 }
