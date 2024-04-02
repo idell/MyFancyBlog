16c6cd4e46c6d51317068849977f702f7c22197a
Ivan Dell'Oro <ivan.delloro@lastminute.com>
2024-04-02 21:31:32 +0200

    configured MyFancyBlogRetrieveController

--- src/main/kotlin/idell/projects/blog/Configuration.kt
+++ src/main/kotlin/idell/projects/blog/Configuration.kt
@@ -1,16 +1,24 @@
 package idell.projects.blog
 
+import idell.projects.blog.crud.common.BlogPostRepository
 import idell.projects.blog.crud.retrieve.controller.BlogCrudRequestAdapter
 import idell.projects.blog.crud.create.usecase.BlogPostUseCase
 import idell.projects.blog.crud.common.InMemoryBlogPostRepository
+import idell.projects.blog.crud.retrieve.usecase.BlogPostRetrieveUseCase
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class Configuration {
+
     @Bean
-    open fun blogPostUseCase() : BlogPostUseCase = BlogPostUseCase(InMemoryBlogPostRepository(mutableMapOf()))
+    open fun blogPostRepository() : BlogPostRepository  =  InMemoryBlogPostRepository(mutableMapOf())
+    @Bean
+    open fun blogPostUseCase(blogPostRepository: BlogPostRepository) : BlogPostUseCase = BlogPostUseCase(blogPostRepository)
 
     @Bean
     open fun blogCrudRequestAdapter() = BlogCrudRequestAdapter()
+
+    @Bean
+    open fun blogPostRetrieveUseCase(blogPostRepository: BlogPostRepository) = BlogPostRetrieveUseCase(blogPostRepository)
 }
--- src/main/kotlin/idell/projects/blog/crud/retrieve/controller/MyFancyBlogRetrieveController.kt
+++ src/main/kotlin/idell/projects/blog/crud/retrieve/controller/MyFancyBlogRetrieveController.kt
@@ -2,26 +2,29 @@ package idell.projects.blog.crud.retrieve.controller
 
 import idell.projects.blog.crud.common.BlogPostKey
 import idell.projects.blog.crud.retrieve.usecase.BlogPostRetrieveUseCase
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 class MyFancyBlogRetrieveController(private val blogPostRetrieveUseCase: BlogPostRetrieveUseCase) {
 
-    @GetMapping("/v1/blog/posts/")
+    @GetMapping("/v1/posts/")
     fun retrieve(@RequestParam(required = false) title: String?,
                  @RequestParam(required = false) category: String?,
-                 @RequestParam(required = false) tags: List<String>?): BlogPostRetrieveResponse {
+                 @RequestParam(required = false) tags: List<String>?): ResponseEntity<BlogPostRetrieveResponse> {
 
         if (title == null && category == null && tags == null) {
-            return EmptyBlogPostsResponse
+            return ResponseEntity.badRequest().build()
         }
         val retrieve = blogPostRetrieveUseCase.retrieve(BlogPostKey(title, category, tags))
         return when {
             retrieve.isEmpty() -> {
-                EmptyBlogPostsResponse
+                ResponseEntity.notFound().build()
             }
 
-            else -> BlogPostsResponse(retrieve.stream().map { BlogPostResponse(it.title, it.content, it.author, it.image, it.category, it.tags) }.toList())
+            else -> ResponseEntity.ok().body(BlogPostsResponse(retrieve.stream().map { BlogPostResponse(it.title, it.content, it.author, it.image, it.category, it.tags) }.toList()))
         }
     }
 
